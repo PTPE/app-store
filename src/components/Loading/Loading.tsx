593b0e9fce3abee4976a5b41545194a8c623a1af
YuJieLeee <yujie@lightup.tw>
2025-03-19 22:31:38 +0800

    feat: use portal for loading ui

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,10 @@
+import { createPortal } from "react-dom";
+
 export default function Loading() {
-  return (
+  return createPortal(
     <div className="fixed flex items-center justify-center w-screen h-screen backdrop-blur-xs">
       <div className="w-8 h-8 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-    </div>
+    </div>,
+    document.body
   );
 }
